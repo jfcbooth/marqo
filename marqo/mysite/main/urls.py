a62f72fc89e059ab63eee31cc307a92224736140
Josh Booth <boothjmail@gmail.com>
2019-03-07 00:27:36 -0500

    added about the team

--- marqo/mysite/main/urls.py
+++ marqo/mysite/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('purpose/', views.purpose, name='purpose'),
+    path('team/', views.team, name='team'),
+    path('contact/', views.contact, name='contact'),
     path('test/', views.test, name='test'),
 ]
--- marqo/mysite/main/views.py
+++ marqo/mysite/main/views.py
@@ -4,5 +4,14 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'main/home.html')
 
+def purpose(request):
+	return render(request, 'main/purpose.html')
+
+def team(request):
+	return render(request, 'main/team.html')
+
+def contact(request):
+	return render(request, 'main/contact.html')
+
 def test(request):
 	return render(request, 'main/test.html')
